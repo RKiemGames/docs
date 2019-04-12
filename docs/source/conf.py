c1e6482338058bd6e566593a30435fe7c35c0633
rdcklinux <rdcklinux@users.noreply.github.com>
2019-04-11 21:55:05 -0400

    configuraciones

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Rdckgmes'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1'
+release = u'1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Rdckgames.tex', u'Rdckgames Documentation',
+    (master_doc, 'Rdckgames.tex', u'Documentación Rdckgames',
      u'Rdckgmes', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'rdckgames', u'Rdckgames Documentation',
+    (master_doc, 'rdckgames', u'Documentación Rdckgames',
      [author], 1)
 ]
 
@@ -149,9 +149,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Rdckgames', u'Rdckgames Documentation',
-     author, 'Rdckgames', 'One line description of project.',
-     'Miscellaneous'),
+    (master_doc, 'Rdckgames', u'Documentación Rdckgames',
+     author, 'Rdckgames', u'Documentación para desarrollo de videojuegos',
+     'Miscelanea'),
 ]
 
 
